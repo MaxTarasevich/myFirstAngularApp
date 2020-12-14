1432bfbeb7a438ad9a5ad6130d381deb02eaadcc
Max <maxtarasevich@gmail.com>
2020-12-14 22:57:37 +0300

    my progect init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
