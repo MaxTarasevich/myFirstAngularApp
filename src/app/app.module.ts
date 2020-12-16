7dd07f2f28b7eaf5ca9341de5effbe45e96807a3
Max <maxtarasevich@gmail.com>
2020-12-16 17:37:10 +0300

    insta header + card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoriesComponent } from './stories/stories.component';
+import { FeedComponent } from './feed/feed.component';
+import { ActionComponent } from './action/action.component';
+import { PostComponent } from './feed/post/post.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    StoriesComponent,
+    FeedComponent,
+    ActionComponent,
+    PostComponent,
    
   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
