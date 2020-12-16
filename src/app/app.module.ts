3a1063aa77ce57bb77d91b641cfe048db204fd47
Max <maxtarasevich@gmail.com>
2020-12-16 13:19:50 +0300

    instagram start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent,
+   
   
   ],
   imports: [
